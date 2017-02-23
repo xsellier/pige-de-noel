e181a60d90f178a6055ad8a5a12d1febf1d92864
Xavier Sellier <xsellier@can2go.com>
2017-02-22 20:33:49 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Pige de noël
+
+## Prerequisite
+
+[NodeJs](https://nodejs.org/en/download/) has to be installed on your computer
+
+## Usage
+
+Extract the attached archive named `pige-de-noel.zip`, then open a terminal.
+
+```shell
+cd pige-de-noel
+
+npm install --production
+node .
+```
+
+Then, load [http://127.0.0.1:8000](http://127.0.0.1:8000) in a browser to see the output
+
+## Linting and testing
+
+Extract the attached archive named `pige-de-noel.zip`, then open a terminal.
+
+```shell
+cd pige-de-noel
+
+npm install 
+npm test
+```
---
+++ test/unit/model/family.js
@@ -0,0 +1,63 @@
+'use strict'
+
+const expect = require('chai').expect
+const path = require('path')
+
+const familyModel = require('../../../lib/model/family')
+
+describe(`${path.relative(process.cwd(), __filename)} experiment`, () => {
+  beforeEach(() => {
+    familyModel.reset()
+  })
+
+  it('Should be able to add a member', (done) => {
+    const member = 'Robert Redford'
+
+    familyModel.addMember(member)
+
+    const addedMember = familyModel.getMember(member)
+
+    expect(addedMember.member).to.be.equal(member)
+    expect(addedMember.spouse).to.not.exist
+
+    done()
+  })
+
+  it('Should be able to add a couple', (done) => {
+    const member = 'Robert Redford'
+    const spouse = 'Sibylle Szaggars'
+
+    familyModel.addCouple(member, spouse)
+
+    const addedMember = familyModel.getMember(member)
+
+    expect(addedMember.member).to.be.equal(member)
+    expect(addedMember.spouse).to.be.equal(spouse)
+
+    const addedSpouse = familyModel.getMember(spouse)
+
+    expect(addedSpouse.member).to.be.equal(spouse)
+    expect(addedSpouse.spouse).to.be.equal(member)
+
+    done()
+  })
+
+  it('Remove a member of a couple, remove only the member', (done) => {
+    const member = 'Robert Redford'
+    const spouse = 'Sibylle Szaggars'
+
+    familyModel.addCouple(member, spouse)
+    familyModel.removeMember(member)
+
+    const removedMember = familyModel.getMember(member)
+
+    expect(removedMember).to.not.exist
+
+    const addedSpouse = familyModel.getMember(spouse)
+
+    expect(addedSpouse.member).to.be.equal(spouse)
+    expect(addedSpouse.spouse).to.not.exist
+
+    done()
+  })
+})
