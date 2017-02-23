cf7f5d4640da56cf9b4adcc4e72100aedffb39cf
Xavier Sellier <xsellier@can2go.com>
2017-02-22 22:29:44 -0500

    Fix unit test for draw-manager, missing callback

--- test/unit/module/draw-manager.js
+++ test/unit/module/draw-manager.js
@@ -7,6 +7,6 @@ const drawManagerModule = require('../../../lib/module/draw-manager')
 
 describe(`${path.relative(process.cwd(), __filename)} experiment`, () => {
   it('Should not be able to get a match until the draw', (done) => {
-    
+    done()
   })
 })
